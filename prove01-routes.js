39906ab912b6cbad666f3d64b2dcb063980e216f
Jersen Meim <51324287+Jersenmeim@users.noreply.github.com>
2021-04-29 13:20:51 +0800

    Week 1 Updated

--- prove01-routes.js
+++ prove01-routes.js
@@ -1,68 +1,31 @@
+const express = require('express');
+const router = express.Router();
 const fs = require('fs');
 
-const requestHandler = (req, res) => {
-    const url = req.url;
-    if (url === '/') {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>Assignment 1</title></head>');
-        res.write('<h1>Create User</h1>');
-        res.write(
-            '<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>'
-        );
-        res.write('</html>');
-        return res.end();
-    }
-    if (url === '/users') {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>Assignment 1</title></head>');
-        res.write('<h1>User List</></h1>');
-        res.write('<body><ul>');
-        res.write('</ul></body>');
-        var i;
+router.get('/', (req, res, next) => {
+    res.render('pages/prove01', {
+        title: 'Prove 01 Assignment',
+        path: '/prove01'
+    })
+});
 
-        const readFileLines = filename =>
-            fs
-            .readFileSync(filename)
-            .toString('UTF8')
-            .split('\n');
+router.get('/users', (req, res, next) => {
+    const list = JSON.parse(fs.readFileSync('names.json'));
+    res.render('pages/user', {
+        title: 'Prove 01 Assignment | User List',
+        path: '/prove01',
+        userData: list
+    })
+});
 
+router.post('/create-user', async (req, res, next) => {
+    const user = req.body.username;
+    const list = JSON.parse(fs.readFileSync('names.json'));
+    list.push(user);
+    fs.writeFileSync('names.json', JSON.stringify(list));
+    console.log(user);
+    // keep user on the original page
+    res.redirect('/');
+})
 
-        // Driver code
-        let arr = readFileLines('users.txt');
-        for (i = 0; i < arr.length; i++) {
-            res.write(`<li> ${arr[i]}</li>`)
-        }
-        res.write('</html>');
-        return res.end();
-    }
-    // Send a HTML response with some "Page not found text
-    if (url === '/create-user') {
-        const body = [];
-        req.on('data', chunk => {
-            body.push(chunk);
-        });
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const usernames = parsedBody.split('=')[1];
-
-            // fs.writeFile('users.txt', JSON.stringify(users), function (err) {
-            //     if (err) return console.log(err);
-
-            // });
-
-            const CreateFiles = fs.createWriteStream('./users.txt', {
-                flags: 'a' //flags: 'a' preserved old data
-            })
-            CreateFiles.write(usernames + '\r\n');
-
-            console.log(usernames); // username=whatever-the-user-entered
-        });
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        res.end();
-    }
-};
-
-exports.handler = requestHandler;
+module.exports = router;
--- prove01.js
+++ prove01.js
@@ -1,8 +1,23 @@
-var express = require('express');
-var app = express();
-const routes = require('./prove01-routes');
-app.get('/', routes.handler);
-app.post('/create-user', routes.handler);
-app.get('/users', routes.handler);
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const PORT = process.env.PORT || 3000 
 
-app.listen(3000);
+const app = express();
+const prove01Routes = require('./prove01-routes');
+
+app.use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .use(express.urlencoded({
+        extended: false
+    }))
+    .use('/', prove01Routes)
+    .use((req, res, next) => {
+        // 404 page
+        res.render('pages/404', {
+            title: '404 - Page Not Found',
+            path: req.url
+        })
+    })
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
