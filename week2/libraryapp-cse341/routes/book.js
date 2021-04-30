eb5efbd591c3c6ad2ae9f504d97dc9fa2e697ed8
Jersen Meim <51324287+Jersenmeim@users.noreply.github.com>
2021-04-30 16:21:27 +0800

    Update book.js

--- week2/libraryapp-cse341/routes/book.js
+++ week2/libraryapp-cse341/routes/book.js
@@ -7,26 +7,31 @@ const books = [];
 
 router.get('/add-book', (req, res, next) => {
     //res.sendFile(path.join(__dirname, '../', 'views/add-book.html'));
-    res.render('add-book', {title: 'add book',});
+    res.render('add-book', {
+        title: 'Library app',
+    });
 });
+
 
 router.post('/add-book', (req, res, next) => {
     const book = req.body.title;
     const summary = req.body.summary;
-    books.push({title: book, summary: summary});
+    books.push({
+        title: book,
+        summary: summary
+    });
 
-    console.log(books);
     res.redirect('/');
 
 });
 
 router.use('/', (req, res, next) => {
 
-   res.render('add-book', {
-    title: 'add book',
-    book: books,
-    path: '/'
-})
+    res.render('add-book', {
+        title: 'Library app',
+        book: books,
+        path: '/'
+    })
 });
 
 exports.routes = router;
